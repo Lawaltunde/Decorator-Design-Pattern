c56df2d7b8105e63a601cca2e1622600df95bf8e
Lawal Tunde <hlawal@mycambrian.ca>
2024-03-25 13:26:02 -0400

    A decorator named PlainDecorator that inherits from the Decorator<string> abstract class

---
+++ DecoratorAssigment/DecoratorAssigment/PlainDecorator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssigment
+{
+    public class PlainDecorator: Decorator<string>
+    {
+        public PlainDecorator(IComponent<string> _component) : base(_component)
+        {
+        }
+
+        public override string GetText()
+        {
+            return string.Concat("The prefix", base.GetText());
+        }
+    }
+}

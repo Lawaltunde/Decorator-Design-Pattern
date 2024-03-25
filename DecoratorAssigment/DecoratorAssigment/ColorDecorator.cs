ffb178d30b939ebb448030cd7a3bbf892975fa1a
Lawal Tunde <hlawal@mycambrian.ca>
2024-03-25 13:33:12 -0400

    A decorator named ColorDecorator that inherits from the Decorator<string> abstract class.

---
+++ DecoratorAssigment/DecoratorAssigment/ColorDecorator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssigment
+{
+    public class ColorDecorator:Decorator<string>
+    {
+        public ColorDecorator(IComponent<string> _component):base(_component) { }
+
+        public override string GetText()
+        {
+            return $"\u001b[31m{base.GetText()}\u001b[0m";
+        }
+    }
+}

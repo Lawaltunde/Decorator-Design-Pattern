7b02597892c96102a6fa57a1b0275aa77fa75bf6
Lawal Tunde <hlawal@mycambrian.ca>
2024-03-26 13:08:06 -0400

    Abstract class Decorator

---
+++ Learning_Decorator_Pattern/Learning_Decorator_Pattern/Decorator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Decorator_Pattern
+{
+    abstract class Decorator<T>:IComponent<T>
+    {
+        protected IComponent<T> component;
+
+        public Decorator(IComponent<T> _component)
+        {
+            component = _component;
+        }
+
+        public T GetText()
+        {
+            return component.GetText();
+        }
+    }
+}

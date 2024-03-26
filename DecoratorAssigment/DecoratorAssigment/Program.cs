bfd1339dd1345e59cf0f147f1dd7ae2076f0062d
Lawal Tunde <hlawal@mycambrian.ca>
2024-03-26 14:17:31 -0400

    Done

--- DecoratorAssigment/DecoratorAssigment/Program.cs
+++ DecoratorAssigment/DecoratorAssigment/Program.cs
@@ -1,12 +1,32 @@
 ﻿
 using DecoratorAssigment;
 
+//instance of ConcreteComponent
 IComponent<string> concreteComponent = new ConcreteComponent();
+
+//instance of UpperCaseDecorator
+
 IComponent<string> upperCaseDecorator = new UpperCaseDecorator(concreteComponent);
+
+//instance of PlainDecorator
+
 IComponent<string> plainDecorator = new PlainDecorator(concreteComponent);
+
+//instance of ColorDecorator
+
 IComponent<string> colorDecorator = new ColorDecorator(concreteComponent);
 
+//calling GetTextMoth on concreteComponent class
 Console.WriteLine(concreteComponent.GetText());
+
+//calling GetTextMoth on upperCaseDecorator class
+
 Console.WriteLine(upperCaseDecorator.GetText());
+
+//calling GetTextMoth on plainDecorator class
+
 Console.WriteLine(plainDecorator.GetText());
+
+//calling GetTextMoth on colorDecorator class
+
 Console.WriteLine(colorDecorator.GetText());
